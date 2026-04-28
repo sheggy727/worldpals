c125f75ff522f01e50d47fd994a631e971a113e6
Shehan Gibson <shehangibson85@gmail.com>
2026-04-28 14:56:06 +0530

    remove node_modules

---
+++ .netlify/functions-internal/___netlify-server-handler/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"lAwrHvAuRIyQuokhbA5e6zFTtLf1Mx96PtMrhNCBkXQ=\"}"
---
+++ .netlify/static/_next/static/chunks/app/(app)/layout-31e260d15e672152.js
@@ -0,0 +1,31 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[814],{7900:function(e,t,r){Promise.resolve().then(r.bind(r,4370))},8030:function(e,t,r){"use strict";r.d(t,{Z:function(){return i}});var a=r(2265);/**
+ * @license lucide-react v0.395.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let n=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase(),s=function(){for(var e=arguments.length,t=Array(e),r=0;r<e;r++)t[r]=arguments[r];return t.filter((e,t,r)=>!!e&&r.indexOf(e)===t).join(" ")};/**
+ * @license lucide-react v0.395.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var c={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
+ * @license lucide-react v0.395.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let l=(0,a.forwardRef)((e,t)=>{let{color:r="currentColor",size:n=24,strokeWidth:l=2,absoluteStrokeWidth:i,className:u="",children:o,iconNode:d,...f}=e;return(0,a.createElement)("svg",{ref:t,...c,width:n,height:n,stroke:r,strokeWidth:i?24*Number(l)/Number(n):l,className:s("lucide",u),...f},[...d.map(e=>{let[t,r]=e;return(0,a.createElement)(t,r)}),...Array.isArray(o)?o:[o]])}),i=(e,t)=>{let r=(0,a.forwardRef)((r,c)=>{let{className:i,...u}=r;return(0,a.createElement)(l,{ref:c,iconNode:t,className:s("lucide-".concat(n(e)),i),...u})});return r.displayName="".concat(e),r}},4042:function(e,t,r){"use strict";r.d(t,{Z:function(){return a}});/**
+ * @license lucide-react v0.395.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let a=(0,r(8030).Z)("Heart",[["path",{d:"M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z",key:"c3ymky"}]])},7138:function(e,t,r){"use strict";r.d(t,{default:function(){return n.a}});var a=r(231),n=r.n(a)},6463:function(e,t,r){"use strict";var a=r(1169);r.o(a,"useParams")&&r.d(t,{useParams:function(){return a.useParams}}),r.o(a,"usePathname")&&r.d(t,{usePathname:function(){return a.usePathname}}),r.o(a,"useRouter")&&r.d(t,{useRouter:function(){return a.useRouter}})},4370:function(e,t,r){"use strict";r.r(t),r.d(t,{default:function(){return m}});var a=r(7437),n=r(998),s=r(6463),c=r(2265),l=r(7138),i=r(8030);/**
+ * @license lucide-react v0.395.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let u=(0,i.Z)("Compass",[["path",{d:"m16.24 7.76-1.804 5.411a2 2 0 0 1-1.265 1.265L7.76 16.24l1.804-5.411a2 2 0 0 1 1.265-1.265z",key:"9ktpf1"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]]);var o=r(4042);/**
+ * @license lucide-react v0.395.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let d=(0,i.Z)("User",[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]]),f=(0,i.Z)("Settings",[["path",{d:"M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z",key:"1qme2f"}],["circle",{cx:"12",cy:"12",r:"3",key:"1v7zrd"}]]);var h=r(3258);function m(e){let{children:t}=e,{data:r,status:l}=(0,n.useSession)(),i=(0,s.useRouter)();return((0,c.useEffect)(()=>{"unauthenticated"===l&&i.push("/login"),"authenticated"!==l||r.user.isOnboarded||i.push("/onboarding")},[l,r,i]),"loading"===l)?(0,a.jsx)(h.A,{}):(0,a.jsxs)("div",{className:"min-h-screen bg-[#FAFAFA] flex flex-col",children:[(0,a.jsx)("main",{className:"flex-1 pb-20",children:t}),(0,a.jsx)(x,{})]})}function x(){let e=(0,s.usePathname)(),t=[{href:"/discover",icon:u,label:"Discover"},{href:"/matches",icon:o.Z,label:"Matches"},{href:"/profile",icon:d,label:"Profile"},{href:"/settings",icon:f,label:"Settings"}];return(0,a.jsx)("nav",{className:"fixed bottom-0 left-0 right-0 bg-white border-t border-gray-100 z-40",children:(0,a.jsx)("div",{className:"max-w-lg mx-auto px-4 py-2 flex justify-around",children:t.map(t=>{let{href:r,icon:n,label:s}=t,c=e===r||e.startsWith(r+"/");return(0,a.jsxs)(l.default,{href:r,className:"flex flex-col items-center gap-1 py-2 px-3 rounded-2xl transition-all ".concat(c?"text-gray-900 bg-primary-50":"text-gray-400 hover:text-gray-600"),children:[(0,a.jsx)(n,{className:"w-6 h-6 ".concat(c?"fill-primary-500 text-gray-900":"")}),(0,a.jsx)("span",{className:"text-xs font-medium ".concat(c?"text-gray-900":""),children:s})]},r)})})})}},3258:function(e,t,r){"use strict";r.d(t,{A:function(){return c},Z:function(){return s}});var a=r(7437);let n={sm:"w-5 h-5",md:"w-8 h-8",lg:"w-10 h-10"};function s(e){let{size:t="md",className:r=""}=e;return(0,a.jsx)("div",{className:"animate-spin border-4 border-primary-500 border-t-transparent rounded-full ".concat(n[t]," ").concat(r)})}function c(){return(0,a.jsx)("div",{className:"min-h-screen flex items-center justify-center",children:(0,a.jsx)(s,{size:"md"})})}}},function(e){e.O(0,[998,231,971,23,744],function(){return e(e.s=7900)}),_N_E=e.O()}]);
